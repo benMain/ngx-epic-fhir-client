5bd406e672190e2b03b7b96626d3b3a1d2acd477
Benjamin Main <bmain@lumeris.com>
2022-04-07 21:43:51 -0500

    refactor(CICD): Setup CI/CD Pipelines.

--- projects/ngx-epic-fhir-client/src/lib/api/localStorage.service.ts
+++ projects/ngx-epic-fhir-client/src/lib/api/localStorage.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class LocalStorageService {
-  get<T extends Record<string, unknown>>(key: string) {
+  get<T>(key: string) {
     const val = localStorage.getItem(key);
     if (!val) {
       return null;
     }
     return JSON.parse(val) as T;
   }
-  set<T extends Record<string, unknown>>(key: string, val: T) {
+  set<T>(key: string, val: T) {
     localStorage.setItem(key, JSON.stringify(val));
   }
   delete(key: string) {
